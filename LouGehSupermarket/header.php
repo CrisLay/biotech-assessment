d058995a53e9e0e244376a3ddced9fe0df330a48
CrisLay <108911486+CrisLay@users.noreply.github.com>
2022-07-12 10:06:09 +0800

    Update header.php

--- LouGehSupermarket/header.php
+++ LouGehSupermarket/header.php
@@ -2,8 +2,8 @@
 <html lang="en">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Luo Geh Supermarket">
+    <meta name="author" content="Criselle Laynes">
     <link rel="stylesheet" href="css/style.css">
 
 
@@ -13,7 +13,7 @@
 <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 
   <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 
 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
